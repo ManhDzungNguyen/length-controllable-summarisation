bc8d79997ac7aa06cabeab4239a4c9abd00a5572
dungnm <nguyenmanhdzung5920@gmail.com>
2023-10-28 11:48:13 +0700

    update evaluate

--- en/function/utils.py
+++ en/function/utils.py
@@ -6,7 +6,27 @@ def calculate_similarity(str1: str, str2: str):
     return fuzz.token_sort_ratio(str1, str2) / 100.0
 
 
-def post_process(raw_summary, remove_duplicated_sentences = False):
+def transform_serial_numbers(text):
+    pattern = r"\[SN\]\s*\d+"
+
+    matches = re.finditer(pattern, text)
+
+    transformed_matches = []
+    for match in matches:
+        match_text = match.group(0)
+        transformed_text = re.sub(r"\]\s*", "]", match_text)
+
+        start, end = match.span()
+        transformed_matches.append((start, end, transformed_text))
+
+    new_text = text
+    for start, end, transformed_text in reversed(transformed_matches):
+        new_text = new_text[:start] + transformed_text + new_text[end:]
+
+    return new_text
+
+
+def post_process(raw_summary, remove_duplicated_sentences=False):
     if raw_summary.startswith("<pad>"):
         raw_summary = raw_summary[5:]
 
@@ -17,7 +37,7 @@ def post_process(raw_summary, remove_duplicated_sentences = False):
     prefix, root = raw_summary.split("[SEP]", 1)
 
     # handle prefix
-    pattern = r'\[SN\]\s*(\d+)'
+    pattern = r"\[SN\]\s*(\d+)"
     matches = re.findall(pattern, prefix)
     if len(matches) != 1:
         return raw_summary
@@ -25,19 +45,19 @@ def post_process(raw_summary, remove_duplicated_sentences = False):
 
     # handle root:
     sentences = root.split("[SN]")
-    sentences = sentences[:no_required_sentences+1]
+    sentences = sentences[: no_required_sentences + 1]
     if len(sentences) < 2:
         return raw_summary
-    
+
     if remove_duplicated_sentences:
         new_sentences = [sentences[0]]
         for sen in sentences[1:]:
             if calculate_similarity(new_sentences[-1], sen) < 0.75:
                 new_sentences.append(sen)
         sentences = new_sentences
-        
-    root = "[SN]".join(sentences)
-    
-    summary = "[SEP]".join([prefix, root])
-    return summary
 
+    root = "[SN]".join(sentences)
+
+    summary = "[SEP]".join([prefix, root])
+    summary = transform_serial_numbers(summary)
+    return summary
--- en/service.sh
+++ en/service.sh
@@ -2,7 +2,7 @@
 while [ "true" ]
 do
     cd /home/kuuhaku/work/length-controllable-summarisation/en
-    source ../venv/bin/activate 
+    source /home/kuuhaku/work/venv/bin/activate 
     python service.py
     sleep 5
 done
